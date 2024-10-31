b67e1677b4054e7e274bd8983ff0d0cc32a435cc
AlejandroRamos4 <not.ronejas@gmail.com>
2024-10-31 17:14:33 +0100

    Modificar perfiles a mitad

--- public/libreria/js/main.mjs
+++ public/libreria/js/main.mjs
@@ -11,8 +11,11 @@ import { InvitadoRegistroPresenter } from "./components/invitado-registro/invita
 import { InvitadoIngresoPresenter } from "./components/invitado-ingreso/invitado-ingreso-presenter.mjs";
 import { ClienteHomePresenter } from "./components/cliente-home/cliente-home-presenter.mjs";
 import { AdminHomePresenter } from "./components/admin-home/admin-home-presenter.mjs";
+import { ClientePerfilPresenter } from "./components/cliente-perfil/cliente-perfil-presenter.mjs";
 
 import { ClienteVerLibroPresenter } from "./components/cliente-ver-libro/cliente-ver-libro-presenter.mjs";
+
+import { AdminPerfilPresenter } from "./components/admin-ver-libro/admin-perfil-presenter.mjs";
 
 export function init() {
   seed();
@@ -28,10 +31,12 @@ export function init() {
 
 
   router.register(/^\/libreria\/admin-agregar-libro.html$/, new AdminAgregarLibroPresenter(model, 'admin-agregar-libro'));
+  router.register(/^\/libreria\/admin-perfil-presenter.html$/, new AdminPerfilPresenter(model, 'admin-perfil'));
   router.register(/^\/libreria\/invitado-ver-libro.html/, new InvitadoVerLibroPresenter(model, 'invitado-ver-libro'));
   router.register(/^\/libreria\/admin-ver-libro.html/, new AdminVerLibroPresenter(model, 'admin-ver-libro'));
 
   router.register(/^\/libreria\/cliente-ver-libro.html/, new ClienteVerLibroPresenter(model, 'cliente-ver-libro'));
+  router.register(/^\/libreria\/cliente-perfil.html$/, new ClientePerfilPresenter(model, 'cliente-perfil'));
 
 
   router.handleLocation();
