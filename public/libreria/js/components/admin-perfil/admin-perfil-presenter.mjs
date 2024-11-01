d80e464fbf144407e243b792bba510c373681bfb
AlejandroRamos4 <not.ronejas@gmail.com>
2024-11-01 19:55:40 +0100

    Perfil admin

---
+++ public/libreria/js/components/admin-perfil/admin-perfil-presenter.mjs
@@ -0,0 +1,129 @@
+import { libreriaSession } from "../../commons/libreria-session.mjs";
+import { Presenter } from "../../commons/presenter.mjs";
+import { router } from "../../commons/router.mjs";
+import { model } from "../../model/model.mjs";
+import { ROL } from "../../model/model.mjs";
+// import { Router } from "../../commons/router.mjs";
+import { MensajesPresenter } from "../mensajes/mensajes-presenter.mjs";
+
+const usuarioActual = JSON.parse(localStorage.getItem('usuarioActual') || '{}');
+
+
+export class AdminPerfilPresenter extends Presenter {
+  constructor(model, view) {
+    super(model, view);
+    this.mensajesPresenter = new MensajesPresenter(model, 'mensajes', '#mensajesContainer');
+  }
+
+
+
+  get modificarButton() { return document.querySelector('#modificarInput'); }
+  get dniInput() { return document.querySelector('#dniInput'); }
+  get dniText() { return this.dniInput.value; }
+  get nombreInput() { return document.querySelector('#nombreInput'); }
+  get nombreText() { return this.nombreInput.value; }
+  get apellidosInput() { return document.querySelector('#apellidosInput'); }
+  get apellidosText() { return this.apellidosInput.value; }
+  get direccionInput() { return document.querySelector('#direccionInput'); }
+  get direccionText() { return this.direccionInput.value; }
+  get emailInput() { return document.querySelector('#emailInput'); }
+  get emailText() { return this.emailInput.value; }
+  get passwordInput() { return document.querySelector('#passwordInput'); }
+  get passwordText() { return this.passwordInput.value; }
+  get rolSelect() { return document.querySelector('#rolSelect'); }
+  get rolText() { return this.rolSelect.value; }
+
+  
+
+  
+  get searchParams(){
+    return new URLSearchParams(document.location.search);
+  }
+
+  get id(){
+    const id = this.searchParams.get('id');
+    console.log("ID capturado:", id);
+    return id;
+  }
+
+  
+
+  async loadUser() {
+    console.log("Ejecutando load...");
+  
+    //const usuarioActual = JSON.parse(localStorage.getItem('usuarioActual') || '{}');
+    const adminID = libreriaSession.getUsuarioId();
+    const user2 = model.getAdminPorId(adminID);
+    
+    console.log(user2);
+
+    
+    if (user2) {
+      this.populateFields(user2);
+      console.log("hola");
+    } else {
+      this.mensajesPresenter.error('User no encontrado');
+      console.error(`User ${adminID} no encontrado!`);
+    }
+  }
+
+  populateFields(mostrarUser) {
+    document.querySelector('#dniInput').value = mostrarUser.dni;
+    document.querySelector('#nombreInput').value = mostrarUser.nombre;
+    document.querySelector('#apellidosInput').value = mostrarUser.apellidos;
+    document.querySelector('#direccionInput').value = mostrarUser.direccion;
+    document.querySelector('#emailInput').value = mostrarUser.email;
+    console.log(mostrarUser);
+    document.querySelector('#passwordInput').value = mostrarUser.password;
+  }
+  
+
+  async modificarAdminClick(event) {
+    event.preventDefault();
+        //Alternativa:
+          /*const usuarioActual = JSON.parse(localStorage.getItem('usuarioActual') || '{}');
+          const user2 = model.getAdministradorPorEmail(usuarioActual.email);*/
+    const adminID = libreriaSession.getUsuarioId();
+    const user2 = model.getAdminPorId(adminID);
+
+    console.log("este es el id:" + user2._id);
+    const adminData = {
+        //_id: user2.id,
+        dni: this.dniInput.value,
+        nombre: this.nombreInput.value,
+        apellidos: this.apellidosInput.value,
+        direccion: this.direccionInput.value,
+        rol: ROL.ADMIN,
+        email: this.emailInput.value,
+        password: this.passwordInput.value
+        
+    };
+
+    try {
+        model.updateAdmin(adminData, adminID); // Llamada al modelo para actualizar el administrador
+        this.mensajesPresenter.mensaje('Administrador modificado!');
+        //alternativa:
+            //Guarda el usuario en localStorage para mantener la sesión activa
+            localStorage.setItem('usuarioActual', JSON.stringify(user2));
+            const usuarioActual2 = JSON.parse(localStorage.getItem('usuarioActual'));
+            console.log(usuarioActual2);
+        
+
+        router.navigate('/libreria/admin-home.html'); // Redireccionar tras modificar
+    } catch (err) {
+        console.log(err);
+        this.mensajesPresenter.error(err.message);
+        await this.mensajesPresenter.refresh();
+    }
+}
+
+async refresh() {
+    console.log("Ejecutando refresh...");
+    await super.refresh();
+    await this.mensajesPresenter.refresh();
+    await this.loadUser();
+    
+    this.modificarButton.onclick = event => this.modificarAdminClick(event); // Asignar la función al botón de modificar
+}
+
+}
--- public/libreria/js/main.mjs
+++ public/libreria/js/main.mjs
@@ -11,6 +11,8 @@ import { InvitadoRegistroPresenter } from "./components/invitado-registro/invita
 import { InvitadoIngresoPresenter } from "./components/invitado-ingreso/invitado-ingreso-presenter.mjs";
 import { ClienteHomePresenter } from "./components/cliente-home/cliente-home-presenter.mjs";
 import { AdminHomePresenter } from "./components/admin-home/admin-home-presenter.mjs";
+import { AdminPerfilPresenter } from "./components/admin-perfil/admin-perfil-presenter.mjs";
+
 
 import { ClienteVerLibroPresenter } from "./components/cliente-ver-libro/cliente-ver-libro-presenter.mjs";
 import { ClienteCarroPresenter } from "./components/cliente-carro/cliente-carro-presenter.mjs";
@@ -23,6 +25,7 @@ export function init() {
   router.register(/^\/libreria$/, new InvitadoHomePresenter(model, 'invitado-home'));
   router.register(/^\/libreria\/cliente-home.html$/, new ClienteHomePresenter(model, 'cliente-home'));
   router.register(/^\/libreria\/admin-home.html$/, new AdminHomePresenter(model, 'admin-home'));
+  
 
   router.register(/^\/libreria\/invitado-ingreso\.html$/, new InvitadoIngresoPresenter(model, 'invitado-ingreso'));
   router.register(/^\/libreria\/invitado-registro\.html$/, new InvitadoRegistroPresenter(model, 'invitado-registro'));
@@ -31,6 +34,7 @@ export function init() {
   router.register(/^\/libreria\/admin-agregar-libro.html$/, new AdminAgregarLibroPresenter(model, 'admin-agregar-libro'));
   router.register(/^\/libreria\/invitado-ver-libro.html/, new InvitadoVerLibroPresenter(model, 'invitado-ver-libro'));
   router.register(/^\/libreria\/admin-ver-libro.html/, new AdminVerLibroPresenter(model, 'admin-ver-libro'));
+  router.register(/^\/libreria\/admin-perfil.html/, new AdminPerfilPresenter(model, 'admin-perfil'));
 
   router.register(/^\/libreria\/cliente-ver-libro.html/, new ClienteVerLibroPresenter(model, 'cliente-ver-libro'));
   router.register(/^\/libreria\/cliente-carro.html$/, new ClienteCarroPresenter(model, 'cliente-carro'))
