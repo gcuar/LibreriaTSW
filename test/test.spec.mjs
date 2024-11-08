179e6c007db112a4bb62cbfa134f52cf3eff24a9
gcuar <gcuar@outlook.es>
2024-11-08 14:15:36 +0100

    test que lee el titulo de un libro nuevo pero creo que este tipo de test no tocan aqui porque ha dicho ricardo en clase que en la p1 son test del navegador y eso es con puppeteer

--- test/test.spec.mjs
+++ test/test.spec.mjs
@@ -2,6 +2,7 @@ import puppeteer from 'puppeteer';
 import { expect } from 'chai';
 import { assert } from 'chai';
 import mocha from 'mocha';
+import { model } from './model.mjs';
 
 describe('Libreria App', function() {
     let browser;
@@ -26,10 +27,31 @@ describe('Libreria App', function() {
     });
 
     it('should display the correct heading', async () => {
-        await page.goto('http://localhost:3000/libreria/index.html'); // Replace with your app's URL
+        await page.goto('http://localhost:3000/libreria/index.html');
         const heading = await page.$eval('h1', el => el.textContent);
         expect(heading).to.equal('Tecnologías y Sistemas Web 24/25'); // Replace with your expected heading
     });
 
     // Add more tests as needed
+
+
+
+// TESTS DE LIBRO 
+    // TESTS: GETTER Y SETTERS 
+    
+    it('Debería asignar y devolver el título de un libro', () => {
+        const libro = model.addLibro({ isbn: "978-84-670-1350-0", titulo: "El Quijote", autores: 'Cervantes', portada: 'https://es.wikipedia.org/wiki/Don_Quijote_de_la_Mancha', resumen: 'Las aventuras de Don Quijote y Sancho Panza por La Mancha', stock: '10', precio: '70'});
+        assert.equal(libro.titulo, 'El Quijote');
+        libro.getTitulo == "El Quijote";
+        assert.equal(libro.titulo, 'El Quijote');
+    });
+ 
+    // TESTS: EXCEPCIONES
+
+
+    // TESTS: AGREGAR, MODIFICAR Y ELIMINAR
+   
+
+    // TESTS: CÁLCULOS
+   
 });
