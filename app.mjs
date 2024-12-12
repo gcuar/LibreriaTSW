54ca39746c21f2c68908344b322e670c98a6c441
gcuar <gcuar@outlook.es>
2024-12-12 12:59:28 +0100

    REACTIVACION DE COMENTARIOS DE LA API

--- app.mjs
+++ app.mjs
@@ -94,7 +94,7 @@ app.post('/api/autenticar', function (req, res, next) {
 //Admins
 
 app.get('/api/admins', function (req, res) {
-  // console.log('/api/admins');
+  console.log('/api/admins');
   try {
     const admins = model.getAdmins(); // Llama al método para obtener los clientes
     res.json(admins); // Envía la lista de clientes como respuesta en formato JSON
@@ -126,7 +126,7 @@ app.delete('/api/admins/:id', function (req, res) {
 });
 
 app.post('/api/admins', function (req, res, next) {
-  // console.log('/api/admins')
+  console.log('/api/admins')
   try {
     let usuario = model.addAdmin(req.body);
     res.json(usuario);
@@ -156,7 +156,7 @@ app.put('/api/admins/:id', function (req, res, next) {
 
 
 app.delete('/api/admins', function (req, res) {
-  // console.log('DELETE /api/admins');
+  console.log('DELETE /api/admins');
 
   try {
     const adminsEliminados = model.removeAdmins(); // Llama al método removeAdmins del modelo
@@ -314,7 +314,7 @@ app.post('/api/admins/autenticar', function (req, res, next) {
   });
   
 app.post('/api/clientes', function (req, res, next) {
-  // console.log('/api/clientes')
+  console.log('/api/clientes')
   try {
     let usuario = model.addCliente(req.body);
     res.json(usuario);
@@ -325,7 +325,7 @@ app.post('/api/clientes', function (req, res, next) {
 })
 
 app.delete('/api/clientes/:id', function (req, res) {
-  // console.log('/api/clientes/:id');
+  console.log('/api/clientes/:id');
   try {
     const id = req.params.id; // Obtener el ID desde los parámetros de la URL
     const clienteEliminado = model.removeCliente(id); // Llamar a la función para eliminar el cliente
@@ -337,7 +337,7 @@ app.delete('/api/clientes/:id', function (req, res) {
 });
 
 app.delete('/api/clientes', function (req, res) {
-  // console.log('DELETE /api/clientes');
+  console.log('DELETE /api/clientes');
   try {
     const clientesEliminados = model.removeClientes(); // Llama al método removeAdmins del modelo
 
